8d68a897b37f7bae4129cd31bae8e10c76616ccd
Njoe00 <nick.james.joe@gmail.com>
2022-11-23 21:20:57 +0000

    added log when connecting to server and user name on snake server

--- client.js
+++ client.js
@@ -2,27 +2,22 @@ const net = require("net");
 
 // establishes a connection with the game server
 const connect = function() {
-   
-
   const conn = net.createConnection({
     host: '165.227.47.243',
-    port: 50541
-
-   
+    port: 50541   
   });
 
   conn.on("connect", () => {
     console.log('Successfully connected to game server');
-    conn.write("NJJ");
-  });
-
-  // interpret incoming data as text
-
-  
-  conn.on("data", (data) => {
-    console.log("data", data);
-  });
-
+    conn.write("Name: NJJ");
+    
+    // interpret incoming data as text
+    
+    
+    conn.on("data", (data) => {
+        console.log("data", data);
+    })
+});
 
 
   conn.setEncoding("utf8");
